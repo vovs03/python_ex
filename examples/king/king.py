edd9c776fad39c6942ba898e65592f14be188962
Vladimir Pavlychev <husky@iMac.local>
2023-02-27 16:25:01 +0300

    Add: Variant f-formating | like Interpolation string.

---
+++ examples/king/king.py
@@ -0,0 +1,13 @@
+castles = 9 + 1
+rooms = 102
+
+king = f"King Balon the {castles}th has {rooms} rooms."
+
+print(king)
+
+
+stark = 'Arya'
+
+# BEGIN (write your solution here)
+print(f'Do you want to eat, {stark}?')
+# END
