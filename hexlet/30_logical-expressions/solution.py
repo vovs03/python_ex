67be67534ae986f4e0032b92bf9aa07ff552ca5e
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-04-23 11:54:03 +0300

    Add: Check string -yes -no

---
+++ hexlet/30_logical-expressions/solution.py
@@ -0,0 +1,27 @@
+# 30 solution.py
+
+# Реализуйте функцию string_or_not(), которая проверяет является ли переданный параметр строкой. Если да, то возвращается строка yes, иначе no.
+
+# string_or_not('Hexlet') # yes
+# string_or_not(10)       # no
+# string_or_not('')       # yes
+# string_or_not(False)    # no
+# Подсказки
+# Проверить, что значение является строкой, можно с помощью функции isinstance():
+
+def string_or_not(val):
+	if isinstance(val, str):
+		print("yes")
+	elif not isinstance(val, str):
+		print("no")
+
+	return val
+
+
+string_or_not("Hex")
+string_or_not("")
+string_or_not(12)
+string_or_not(True)
+
+# isinstance(3, str) # False
+# isinstance('Hexlet', str) # True
