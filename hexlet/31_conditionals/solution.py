40f032fac3107c5dd2bc6655a577a99b22b85ee3
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-04-26 10:35:52 +0300

    UPD

--- hexlet/31_conditionals/solution.py
+++ hexlet/31_conditionals/solution.py
@@ -1,67 +1,19 @@
-# solution.py
+def normalize_url(url):
 
-def normalize_url(address):
-    normal = address
-
-    #############
-    # CASE 1
-
-    # prtcl = 'https://'
-    # #if address[:8] != 'https://' or address[:7] != 'http://':
-    # if address[:8] != 'https://':
-    #     normal = prtcl + address
-    # elif address[:7] == 'http://':
-    # 	normal = prtcl + address[7:]
+    https = 'https://'
     
-    # if address[:7] != 'http://'
-    # 	normal = 
-    # return normal
+    #Check HTTPS_ADDRESS
+    if url[:8] == 'https://':
+        return url
 
-    # http:// 
-    # return 'https://' + address[7:] if address[:7] != 'http://' or 
+    #Check HTTP_ADDRESS
+    elif url[:7] == 'http://':
+        return https + url[7:]
 
-    #############
-    # CASE 2
-
-    """
-    # Just // http://king
-    if address[:7] == 'http://':
-        # print("*** " * 5)
-        # print('https://' + address[7:])
-
-        normal = 'https://' + address[7:]
-
-    # Just // kong
-    elif address[:6] != 'http://' and address[:7] != 'https://':
-        normal = 'https://' + address
-
-
-    # Just // https://king-kong-film.cinema
-    elif address[:7] == 'https://':
-        normal = address[:]
-        print("это норм")
-    """ # 13:42
-
-    #############
-    # CASE 3
-
-    prtcl = 'https://'
-    #normal = prtcl + address
-
-    # adr
-    # kong !h !hs
+    #Check ADDRESS
+    else:
+        return https + url
     
-    if address[:7] != 'http://' \
-        and address[:8] != 'https://':
-        normal = prtcl + address
-    elif address[:7] == 'http://':
-        normal = prtcl + address[7:]
-    elif address[:8] == 'https://':
-        normal = address
-    # it's work 14:04
-
-    return normal
-
 
 print(normalize_url('kong'))
 print(normalize_url('http://king'))
