a083a07644193bb0bc0662382dc98f4542fb0b16
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-05-09 11:36:39 +0300

    add: aggregation

--- hexlet/34_aggregation/solution.py
+++ hexlet/34_aggregation/solution.py
@@ -11,12 +11,15 @@
 
 def join_numbers_from_range(num1, num2):
 
-	# получить список чисел
-	ll = list(range(num1,num2 + 1))
-
-	res = "".join(map(str, ll))
+	i = num1
+	res = ''
+	while i <= num2:
+		res = res + str(i)
+		i += 1
 	return res
 
 
-print(join_numbers_from_range(5, 10))
+
 print(join_numbers_from_range(1, 1))
+print(join_numbers_from_range(2, 3))
+print(join_numbers_from_range(5, 10))
